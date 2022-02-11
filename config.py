dde712d5cd930dba7ad211b2a7f2bf67be8eca74
sheldonldev <xiadanli0320@gmail.com>
2022-02-12 04:48:12 +0800

    .

--- config.py
+++ config.py
@@ -7,55 +7,177 @@ PORT = config("PORT")
 
 
 server_config = {
-    "inbounds": [{
-        "port": int(PORT),
-        "protocol": "vmess",
-        "settings": { "clients": [{ "id": f"{UUID.lower()}" }] }
-    }],
-    "outbounds": [{
-        "protocol": "freedom",
-        "settings": {}
-    }]
+  "log": {
+    "loglevel": "warning",
+    "access": "/var/log/v2ray/access.log",
+    "error": "/var/log/v2ray/error.log"
+  },
+  "stats": {},
+  "api": {
+    "tag": "api",
+    "services": ["StatsService"]
+  },
+  "policy": {
+    "levels": {
+      "0": {
+        "statsUserUplink": true,
+        "statsUserDownlink": true
+      }
+    },
+    "system": {
+      "statsInboundUplink":true,
+      "statsInboundDownlink": true
+    }
+  },
+  "inbounds": [
+    {
+      "sniffing": {
+        "enabled": true,
+        "destOverride": ["http", "tls"]
+      },
+      "tag": "tcp",
+      "port": int(PORT),
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "email": "99999999@muppets.monster",
+            "id": f"{UUID}",
+            "level": 0,
+            "alterId": 64
+          }
+        ]
+      }
+    },
+    {
+      "listen": "127.0.0.1",
+      "port": 10085,
+      "protocol": "dokodemo-door",
+      "settings": {
+        "address": "127.0.0.1"
+      },
+      "tag": "api"
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "block"
+    }
+  ],
+  "routing": {
+    "domainStrategy": "AsIs",
+    "rules": [
+      {
+        "type": "field",
+        "outboundTag": "block",
+        "protocol": ["bittorrent"]
+      }
+    ],
+    "settings": {
+      "rules": [
+        {
+          "inboundTag": ["api"],
+          "outboundTag": "api",
+          "type": "field"
+        }
+      ]
+    },
+    "strategy": "rules"
+  }
 }
 
 client_config = {
-    "inbounds": [{
-        "port": 10808,
-        "listen": "127.0.0.1",
-        "protocol": "socks",
-        "settings": {
-            "udp": True
-        }
-    },{
-        "port": 10809,
-        "listen": "127.0.0.1",
-        "protocol": "http",
-        "settings": {
-            "udp": True
-        }
-    }],
-    "outbounds": [{
-        "protocol": "vmess",
-        "settings": {
-        "vnext": [{
-            "address": f"{ADDRESS}",
-            "port": int(PORT),
-            "users": [{ "id": f"{UUID.lower()}" }]
-        }]
-        }
-    },{
-        "protocol": "freedom",
-        "tag": "direct",
-        "settings": {}
-    }],
-    "routing": {
-        "domainStrategy": "IPOnDemand",
-        "rules": [{
-        "type": "field",
-        "ip": ["geoip:private"],
-        "outboundTag": "direct"
-        }]
+  "log": {
+    "error": "",
+    "loglevel": "info",
+    "access": ""
+  },
+  "inbounds": [
+    {
+      "listen": "127.0.0.1",
+      "protocol": "socks",
+      "settings": {
+        "udp": false,
+        "auth": "noauth"
+      },
+      "port": "10808"
+    },
+    {
+      "listen": "127.0.0.1",
+      "protocol": "http",
+      "settings": {
+        "timeout": 360
+      },
+      "port": "10809"
     }
+  ],
+  "outbounds": [
+    {
+      "mux": {
+        "enabled": false,
+        "concurrency": 8
+      },
+      "protocol": "vmess",
+      "streamSettings": {
+        "network": "tcp",
+        "tcpSettings": {
+          "header": {
+            "type": "none"
+          }
+        },
+        "security": "none"
+      },
+      "tag": "proxy",
+      "settings": {
+        "vnext": [
+          {
+            "address": f"{ADDRESS}",
+            "users": [
+              {
+                "id": f"{UUID}",
+                "alterId": 0,
+                "level": 0,
+                "security": "aes-128-gcm"
+              }
+            ],
+            "port": int(PORT)
+          }
+        ]
+      }
+    },
+    {
+      "tag": "direct",
+      "protocol": "freedom",
+      "settings": {
+        "domainStrategy": "UseIP",
+        "userLevel": 0
+      }
+    },
+    {
+      "tag": "block",
+      "protocol": "blackhole",
+      "settings": {
+        "response": {
+          "type": "none"
+        }
+      }
+    }
+  ],
+  "dns": {},
+  "routing": {
+    "settings": {
+      "domainStrategy": "AsIs",
+      "rules": []
+    }
+  },
+  "transport": {}
 }
 
 with open('/usr/local/etc/v2ray/config.json', 'w') as f:
